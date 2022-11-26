86e101657e40c3ac70cae5942ab1adde4ab60469
Jack McMorrow <jmcmorrow08@gmail.com>
2022-11-26 11:45:01 -0500

    python file commit

---
+++ team9data.py
@@ -0,0 +1,21 @@
+# Team Nine Final Project
+# 
+# 
+# %%
+## IMPORTING LIBRARIES
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns 
+
+import statsmodels as sm
+
+# %%
+## IMPORTING DATA
+data = pd.read_csv("Team9 data.csv")
+data.head()
+# %%
+data.shape # 84,548 rows and 22 columns
+# %%
+data.describe()
+# %%
