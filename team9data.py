3fd9f7068c20beb9a5437e8459b0844af419420a
Udbhav Kush <ukush4@gmail.com>
2022-11-29 00:04:45 -0500

    basic data analysis

--- team9data.py
+++ team9data.py
@@ -12,10 +12,18 @@ import statsmodels.api as sm
 
 # %%
 ## IMPORTING DATA
-data = pd.read_csv("Team9 data.csv")
+data = pd.read_csv("Team9_data.csv")
 data.head()
 # %%
 data.shape # 84,548 rows and 22 columns
 # %%
 data.describe()
+# %%
+data.nunique()
+
+# %%
+data['TAX CLASS AT PRESENT'].value_counts()
+# %%
+class_sale_count = data['BUILDING CLASS AT TIME OF SALE'].value_counts()
+class_sale_count
 # %%
